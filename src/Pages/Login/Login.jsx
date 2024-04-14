9f6078de2ff5650904036c11e36112e94e9f390d
Sayem <sayem465508@gmail.com>
2024-04-14 08:35:54 +0600

    added login route

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,9 @@
+import Navbar from "../Shared/Navbar/Navbar";
 
 const Login = () => {
     return (
         <div>
+            <Navbar />
             <h2 className="text-3xl">This is Login</h2>
         </div>
     );
